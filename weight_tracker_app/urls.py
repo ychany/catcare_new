cbe88c49a20a862694c505bd7339886047169c6f
joyeongchan <tigerbone@naver.com>
2025-07-23 02:36:35 +0900

    carcare_new figma

--- weight_tracker_app/urls.py
+++ weight_tracker_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'weight_tracker_app'
+
 urlpatterns = [
     path('', views.weight_tracker_view, name='weight_tracker'),
     path('api/weights/', views.weight_list, name='weight_list'),
